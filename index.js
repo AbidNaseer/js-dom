6e3d3b361e74bc963d4c8904893d75fc50616009
abid naseer <abidnaseer454@gmail.com>
2024-03-20 13:27:49 +0500

    debuging done

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const form = document.querySelector('form');
-    
-    form.addEventListener('submit', function (e){
-        e.preventDefault()
-        const yourName = document.querySelector("#yName").value;
-        const loverName = document.querySelector("#lName").value;
-        const result = document.querySelector("#result");
-        const random = Math.floor(Math.random() * 100);
+const form = document.querySelector("form");
 
-        if (yourName && loverName == "") {
-            result.innerHTML = `enter a valid user name`
-        }else{
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const yourName = document.querySelector("#yName").value;
+  const loverName = document.querySelector("#lName").value;
+  const result = document.querySelector("#result");
+  const random = Math.floor(Math.random() * 100);
 
+
+      if (yourName && loverName !== "" ) {
         result.innerHTML = `${yourName} your precentage with ${loverName} is ${random} %`
-    }});
-    
-
+        setInterval(() => {
+            location.reload()
+          }, 3000);
+        }
+        else{
+          result.innerHTML = `enter a valid user name`
+          setInterval(() => {
+            location.reload()
+          }, 2000);
+        }
+});
